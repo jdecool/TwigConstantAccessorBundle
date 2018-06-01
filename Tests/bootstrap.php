afa4aeab2ec9e7e7c8077b60cf14546bbcaf4433
Jérémy DECOOL <contact@jdecool.fr>
2018-06-01 11:14:39 +0200

    Fix tests (error with matthiasnoback/symfony-dependency-injection-test dependency)

---
+++ Tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+// PHPUnit compatibility
+if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
+    class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
+}
